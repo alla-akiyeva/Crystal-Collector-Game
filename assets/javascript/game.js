20f2c32098d76141d736a61a0858824c08d3b3b2
Alla Akiyeva <alla.akiyeva@gmail.com>
2019-03-24 19:29:22 -0400

    saving progress

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+
+// Creating variables that hold references to the places in the HTML where we want to display things.
+var currentWordText = document.getElementById("current-word-text");
+var wordChosenText = document.getElementById("word-place-holder");
+var guessesText = document.getElementById("guesses-text");
+var wrongGuessesText = document.getElementById("wrong-guesses");
+
+// // Creating variables that hold references to the places in the HTML where we want to display things.
+var randomNum = document.getElementById("random-number");
+var wins = document.getElementById("wins");
+var losses = document.getElementById("losses");
+var score = document.getElementById("score");
