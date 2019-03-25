2bbe7cb5ade3de45a249ad3de2b7b24413e75c2d
Alla Akiyeva <alla.akiyeva@gmail.com>
2019-03-24 22:43:34 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,23 @@
 
-// Creating variables that hold references to the places in the HTML where we want to display things.
-var currentWordText = document.getElementById("current-word-text");
-var wordChosenText = document.getElementById("word-place-holder");
-var guessesText = document.getElementById("guesses-text");
-var wrongGuessesText = document.getElementById("wrong-guesses");
-
 // // Creating variables that hold references to the places in the HTML where we want to display things.
-var randomNum = document.getElementById("random-number");
-var wins = document.getElementById("wins");
-var losses = document.getElementById("losses");
-var score = document.getElementById("score");
+var randomNumText = document.getElementById("random-number");
+var winsText = document.getElementById("wins");
+var lossesText = document.getElementById("losses");
+var scoreText = document.getElementById("score");
+
+var score = 0;
+var wins = 0;
+var losses = 0;
+
+$("document").load(reset() {
+    // Generating a random number that the user will be matching
+    var randomNum = Math.floor(Math.random() * (120 - 19))+ 19;
+
+    // Generating random number between 1 and 12 for each crystal
+    var crystal1 = Math.floor(Math.random() * (12 - 1))+ 1;
+    var crystal2 = Math.floor(Math.random() * (12 - 1))+ 1;
+    var crystal3 = Math.floor(Math.random() * (12 - 1))+ 1;
+    var crystal4 = Math.floor(Math.random() * (12 - 1))+ 1;
+
+    var score = 0;
+});
