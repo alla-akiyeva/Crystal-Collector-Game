6e233a00c100bed5f0e7f003fcee3c72bc2983d0
Alla Akiyeva <alla.akiyeva@gmail.com>
2019-03-25 01:28:53 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,71 @@
 
-// // Creating variables that hold references to the places in the HTML where we want to display things.
-var randomNumText = document.getElementById("random-number");
-var winsText = document.getElementById("wins");
-var lossesText = document.getElementById("losses");
-var scoreText = document.getElementById("score");
-
+// Define variables
 var score = 0;
 var wins = 0;
 var losses = 0;
 
-$("document").load(reset() {
+// Function to generate random numbers at the start of the game.
+function randomGenerator () {
     // Generating a random number that the user will be matching
-    var randomNum = Math.floor(Math.random() * (120 - 19))+ 19;
+    randomNum = Math.floor(Math.random() * (120 - 19))+ 19;
+    $('#random-number').html(randomNum);
 
     // Generating random number between 1 and 12 for each crystal
-    var crystal1 = Math.floor(Math.random() * (12 - 1))+ 1;
-    var crystal2 = Math.floor(Math.random() * (12 - 1))+ 1;
-    var crystal3 = Math.floor(Math.random() * (12 - 1))+ 1;
-    var crystal4 = Math.floor(Math.random() * (12 - 1))+ 1;
+    crystal1 = Math.floor(Math.random() * (12 - 1))+ 1;
+    crystal2 = Math.floor(Math.random() * (12 - 1))+ 1;
+    crystal3 = Math.floor(Math.random() * (12 - 1))+ 1;
+    crystal4 = Math.floor(Math.random() * (12 - 1))+ 1;
+};
 
-    var score = 0;
-});
+// Function to reset the game
+function reset () {
+    score = 0;
+    $("#score").html(score);
+    randomGenerator();
+}
+
+// Function to check if User matched the score to the random number
+function conditions () {
+    if (score == randomNum) {
+        wins++;
+        $("#wins").html("Wins: " + wins);
+        $("#winOrLoss").html("You won!");
+        reset();
+    } else if (score > randomNum) {
+        losses++;
+        $("#losses").html("Losses: " + losses);
+        $("#winOrLoss").html("You lost!");
+        reset();
+    }
+}
+
+// Creating a function to update the score as each button is clicked.
+function crystal1Click () {
+    score += crystal1;
+    $('#score').html(score);
+    conditions ();
+}
+function crystal2Click () {
+    score+= crystal2;
+    $('#score').html(score);
+    conditions ();
+}
+function crystal3Click () {
+    score += crystal3;
+    $('#score').html(score);
+    conditions ();
+}
+function crystal4Click () {
+    score += crystal4;
+    $('#score').html(score);
+    conditions ();
+}
+
+// Generating random numbers at the start of the game by calling randomGenerator function
+randomGenerator();
+
+// Calling on-click functions
+$('#crystal1').on('click', crystal1Click);
+$('#crystal2').on('click', crystal2Click); 
+$('#crystal3').on('click', crystal3Click); 
+$('#crystal4').on('click', crystal4Click); 
